7cc15d3dadd1095605ee94f79e2aed98f3179229
Gomez <hag07@my.fsu.edu>
2018-10-21 22:29:42 -0400

    email optional and keep text if failed

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      
-
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Signup Page</title>
-    </head>
-    <body>
-        <h1>Signup</h1>
-"""
-
-page_footer = """
-    </body>
-</html>
-"""
+app.config['DEBUG'] = True
+app.secret_key = 'abcdefghijklmnopqrstuvwyz'      
 
 # it contains a space character or it consists of less than 3 characters or more than 20 characters
-@app.route("/signup", methods=['POST'])
-def add_user():
+
+
+@app.route("/signup", methods=['GET','POST'])
+def sign_up():
     new_user = request.form['username']
     new_pass = request.form['password']
-    error_username = "Not valid username"
-    error_password = "Not valid password"
-    if new_user == "" or len(new_user) < 3 or len(new_user) >20 or " " in new_user:
-        return redirect("/?error=" + error_username)
-    if new_pass == "" or len(new_pass) < 3 or len(new_pass) >20 or " " in new_pass:   
-        return redirect("/?error=" + error_password)
-    else: 
-        return render_template('welcome.html', new_user=new_user)
+    verify_password = request.form['verify']
+    new_email = request.form['email']
       
+    if new_user == "" or len(new_user) < 3 or len(new_user) >20 or " " in new_user:
+        flash('Username not valid','us')
+        #return redirect('/')
+    if new_pass == "" or len(new_pass) < 3 or len(new_pass) >20 or " " in new_pass:
+        flash('Password not valid','pw')
+        #return redirect('/')   
+    if verify_password != new_pass or verify_password == "":
+        flash('Passwords do not match','verify')
+        #return redirect('/')
+    if " " in new_email or "@" not in new_email or "." not in new_email:
+        flash('Email not valid','email')
+        return redirect('/')
+    else: 
+        return render_template('welcome.html', new_user=new_user)         
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    encoded_error = request.args.get("error")
-    return render_template('forms.html', error=encoded_error)
-
+     new_user = request.form.get('username')
+     return render_template('forms.html', new_user=new_user)
+     
 
 app.run()
+
+
